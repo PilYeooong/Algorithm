1feb8761559cafe3286727c685ed949255ac86fa
PilYeong <pilyeooong@gmail.com>
2021-04-15 12:13:25 +0900

    ADD: solved cases

--- Baekjoon/DP/1149.py
+++ Baekjoon/DP/1149.py
@@ -6,4 +6,5 @@ for i in range(1, N+1):
     dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + a[i][0]
     dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + a[i][1]
     dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + a[i][2]
+
 print(min(dp[N][0], dp[N][1], dp[N][2]))
