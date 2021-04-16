02b53031351756d7b88236649f01f0cb8c84221e
PilYeong <pilyeooong@gmail.com>
2021-04-16 16:43:40 +0900

    ADD: solved cases

--- Baekjoon/DP/11057.py
+++ Baekjoon/DP/11057.py
@@ -1,15 +1,14 @@
 N = int(input())
 
-dp = [[for _ in range(10)] for _ in range(1001)]
+dp = [[0 for _ in range(10)] for _ in range(1001)]
 
 for i in range(10):
     dp[1][i] = 1
 
 for i in range(2, N+1):
     for j in range(10):
-        if j == 0:
-            dp[i][j] = sum(dp[i-1])
-        elif j == 9:
-            dp[i][j] = 1
-        else:
-            dp[i][j] = dp[i][j-1] - dp[i-1][j-1]
+        for k in range(j+1):
+            dp[i][j] += dp[i-1][k]  # 맨 마지막자리 j를 없앴을때 올수있는 앞자리의 모든 경우의수를 더한다.
+            dp[i][j] %= 10007
+ans = sum(dp[N])
+print(ans % 10007)
